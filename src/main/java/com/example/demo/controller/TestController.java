82ffbb98fb9014cdebae42afa039889d3dd71057
김현규 <khk15888@naver.com>
2025-05-08 12:30:18 +0900

    fragment End

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+	@GetMapping("/index")
+	public String index() {
+		return "index";
+	}
+
+	@GetMapping("/test")
+	public String test() {
+		return "test";
+	}
+
+}
